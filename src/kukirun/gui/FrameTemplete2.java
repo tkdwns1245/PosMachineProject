a151eb9f7c5d5c7ce9c5bfd557847f4f712ee829
DESKTOP-7OPB0LV\toosk <windxtoto123@gmail.com>
2021-01-14 20:36:21 +0900

    템플릿 객체에 있던 resize,resizegui,helpgui 를 CreateObject 객체에 병합. 새 템플릿에서 CreateObject를 활용 할 때 반복 작성을 줄여 편의성 증대.

--- src/kukirun/gui/FrameTemplete2.java
+++ src/kukirun/gui/FrameTemplete2.java
@@ -27,85 +27,32 @@ import java.awt.*;
 
 
 public abstract class FrameTemplete2 extends JFrame { 
-	Container con;
+	static Container con;
 	static int width=1000;
 	static int height=600;
 	
-	CreateObject CO=new CreateObject();
+
+	
 	
 	// Component º¯¼ö ¼±¾ð
 
 	JPanel mainPanel= new JPanel();
-	JPanel loginPanel= CO.setPanel(300,150,350,153);
-	JPanel titlePanel= CO.setPanel(400,50, 300,30);
+	JPanel initPanel= new JPanel();
+	CreateObject CO=new CreateObject(mainPanel,width,height,initPanel);
 	
-	JLabel title=CO.setLabel			(300, 100, 400, 50, "POSmachine");
-	JLabel id=CO.setLabel				(100, 20, 60, 30, "¾Æ ÀÌ µð  : ");
-	JLabel password=CO.setLabel			(100, 20, 60, 70, "ÆÐ½º¿öµå : ");
-	JTextField idTF=CO.setTextField		(100, 20, 150, 30, 10);
-	JTextField passwordTF=CO.setTextField(100, 20, 150, 70, 10);
 
-	JButton loginBtn=CO.setButton(90, 30, 40,110, "·Î±×ÀÎ");	
-	JButton regiBtn=CO.setButton(90, 30, 150, 110, "È¸¿ø°¡ÀÔ"); 
-	JButton exitBtn=CO.setButton(90, 30, 850, 500, "Á¾·á");
+	JPanel loginPanel= CO.setPanel(300,150,350,153,mainPanel);
+	JPanel titlePanel= CO.setPanel(400,50, 300,30,mainPanel);
 	
-	
-	public void guiHelp()
-	{
-		loginPanel.addMouseListener(CO.selectObject(loginPanel,mainPanel));
-		titlePanel.addMouseListener(CO.selectObject(titlePanel,mainPanel));
-		
-		title.addMouseListener(CO.selectObject(title,mainPanel));
-		
-		id.addMouseListener(CO.selectObject(id,loginPanel));
-		password.addMouseListener(CO.selectObject(password,loginPanel));
-		idTF.addMouseListener(CO.selectObject(idTF,loginPanel));
-		passwordTF.addMouseListener(CO.selectObject(passwordTF,loginPanel));
-		loginBtn.addMouseListener(CO.selectObject(loginBtn,loginPanel));
-		regiBtn.addMouseListener(CO.selectObject(regiBtn,loginPanel));
-		
-		exitBtn.addMouseListener(CO.selectObject(exitBtn,mainPanel));
-		
-		this.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println(getMousePosition());
-			}
-		});
-	}
+	JLabel title=CO.setLabel			(300, 100, 400, 50, "POSmachine",mainPanel,20);
+	JLabel id=CO.setLabel				(100, 20, 60, 30, "¾Æ ÀÌ µð  : ",loginPanel,13);
+	JLabel password=CO.setLabel			(100, 20, 60, 70, "ÆÐ½º¿öµå : ",loginPanel,13);
+	JTextField idTF=CO.setTextField		(100, 20, 150, 30, 10,loginPanel);
+	JTextField passwordTF=CO.setTextField(100, 20, 150, 70, 10,loginPanel);
 
-	
-	public void resizeGui()
-	{
-		resize(titlePanel,width*4/10,height*5/60, width*3/10,height/20);
-		resize(loginPanel,width*3/10,height/4, width*7/20,height*9/40);
-		
-		
-		resize(loginBtn,width/100*9, height/20, width/25, height/60*11);
-		loginBtn.setFont(new Font("¸¼Àº °íµñ",Font.BOLD, 12*width/1000));
-		
-		resize(regiBtn,width/100*9, height/20, width/5, height/60*11);
-		regiBtn.setFont(new Font("¸¼Àº °íµñ",Font.BOLD, 12*width/1000));
-		
-		resize(exitBtn,width/100*9, height/20, width/20*17, height/6*5);
-		exitBtn.setFont(new Font("¸¼Àº °íµñ",Font.BOLD, 12*width/1000));
-		
-		
-		
-		resize(title,width*3/10, height/6, width*4/10, height/12);
-		title.setFont(new Font("¸¼Àº °íµñ",Font.BOLD, 20*width/1000));
-		
-		resize(id,width/10, height/30, width*6/100, height*3/60);
-		id.setFont(new Font("¸¼Àº °íµñ",Font.PLAIN, 13*width/1000));
-		
-		resize(password,width/10, height/30, width*6/100, height*7/60);
-		password.setFont(new Font("¸¼Àº °íµñ",Font.PLAIN, 13*width/1000));
-		
-		resize(idTF,width/10, height/30, width/20*3, height/20);
-		resize(passwordTF,width/10, height/30, width/20*3, height/60*7);
-
-	}
+	JButton loginBtn=CO.setButton(90, 30, 40,110, "·Î±×ÀÎ",loginPanel,12);	
+	JButton regiBtn=CO.setButton(90, 30, 150, 110, "È¸¿ø°¡ÀÔ",loginPanel,12); 
+	JButton exitBtn=CO.setButton(90, 30, 850, 500, "Á¾·á",mainPanel,12);
 	
 	
 	public void addGui()
@@ -137,7 +84,15 @@ public abstract class FrameTemplete2 extends JFrame {
 		loginPanel.add(passwordTF);
 		loginPanel.add(loginBtn);
 
-		loginPanel.add(regiBtn);		
+		loginPanel.add(regiBtn);	
+		
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(getMousePosition());
+			}
+		});
 	}
 	
 
@@ -168,6 +123,19 @@ public abstract class FrameTemplete2 extends JFrame {
 
 		}
 	});
+		
+		regiBtn.addActionListener(new ActionListener() {			
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println(mainPanel.getComponent(3));
+
+		}
+	});
+		
+		
+		
+		
 	}
 
 
@@ -181,50 +149,31 @@ public abstract class FrameTemplete2 extends JFrame {
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		this.setSize(width, height);
+		initPanel.setSize(width, height);
 		
-		this.addComponentListener(new ComponentAdapter() {
 
-			
-			@Override
-			public void componentResized(ComponentEvent e) {
-				// TODO Auto-generated method stub
-				
-				height=getHeight();
-				width=getWidth();
-				
-				resizeGui();
-				//showGui();
-			}
-			
-		});
+
+//		this.addComponentListener(new ComponentAdapter() {
+//
+//			
+//			@Override
+//			public void componentResized(ComponentEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//				height=getHeight();
+//				width=getWidth();
+//				
+//				resizeGui();
+//				//showGui();
+//			}
+//			
+//		});
 		
 
 		
 	}  
 	
-	public void resize(JPanel hey,int sizeX,int sizeY,int coordiX,int coordiY)
-	{
-		hey.resize(sizeX,sizeY);
-		hey.move(coordiX,coordiY);
-		
-	}
-	public void resize(JLabel hey,int sizeX,int sizeY,int coordiX,int coordiY)
-	{
-		hey.resize(sizeX,sizeY);
-		hey.move(coordiX,coordiY);
-	}
-	public void resize(JButton hey,int sizeX,int sizeY,int coordiX,int coordiY)
-	{
-		hey.resize(sizeX,sizeY);
-		hey.move(coordiX,coordiY);
-	
-	}
-	public void resize(JTextField hey,int sizeX,int sizeY,int coordiX,int coordiY)
-	{
-		hey.resize(sizeX,sizeY);
-		hey.move(coordiX,coordiY);
-	}
-	
+
 
 	public abstract void CreateComponent();
 }
